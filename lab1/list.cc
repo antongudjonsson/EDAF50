383e59b962d9d08f887b3152ff31699f415af32e
antongudjonsson <anton_gudjonsson@hotmail.com>
2019-02-04 17:02:08 +0100

    Completed assignment 4 Implemented the methods in list.cc

--- lab1/list.cc
+++ lab1/list.cc
@@ -1,30 +1,124 @@
 #include <iostream>
 #include "list.h"
 
+using std::cout;
+using std::endl;
+
 List::List() {
+	first = nullptr;
 }
 
 List::~List() {
 }
 
 bool List::exists(int d) const {
-	return true;
+	if(empty()){
+		return false;
+	}else{
+		Node* temp = first;
+		while(temp != nullptr){
+			if(temp->value == d){
+				return true;
+			}
+			temp = temp->next;
+		}
+		return false;
+	}
 }
 
 int List::size() const {
-	return 0;
+	Node* temp = first;
+	int count = 0;
+	while(temp != nullptr){
+		count++;
+		temp = temp->next;
+	}
+	return count;
 }
 
 bool List::empty() const {
-	return true;
+	if(first == nullptr){
+		return true;
+	}else{
+		return false;
+	}
 }
 
 void List::insertFirst(int d) {
+	Node* myNode = new Node(d, first);
+	first = myNode;
 }
 
 void List::remove(int d, DeleteFlag df) {
+	Node* thisNode = first;
+	Node* prevNode = nullptr;
+	switch (df)
+	{
+		case List::DeleteFlag::EQUAL:
+			while(thisNode->next != nullptr){
+				if(thisNode->value == d){
+					if(prevNode == nullptr){
+						first = thisNode->next;
+					}else{
+						prevNode->next = thisNode->next;
+					}
+					delete thisNode;
+					break;
+				}
+				prevNode = thisNode;
+				thisNode = thisNode->next;
+			}
+			break;
+		
+		case List::DeleteFlag::GREATER:
+			while(thisNode->next != nullptr){
+				if(thisNode->value > d){
+					if(prevNode == nullptr){
+						first = thisNode->next;
+					}else{
+						prevNode->next = thisNode->next;
+					}
+					delete thisNode;
+					break;
+				}
+				prevNode = thisNode;
+				thisNode = thisNode->next;
+			}
+			break;
+
+		case List::DeleteFlag::LESS:
+			while(thisNode->next != nullptr){
+				if(thisNode->value < d){
+					if(prevNode == nullptr){
+						first = thisNode->next;
+					}else{
+						prevNode->next = thisNode->next;
+					}
+					delete thisNode;
+					break;
+				}
+				prevNode = thisNode;
+				thisNode = thisNode->next;
+			}
+			break;
+
+		default:
+			cout << "Not valid delete flag" << endl;
+	}
+
 }
 
 void List::print() const {
+	if(!empty()){
+		Node* myNode = first;
+		std::cout << std::endl << "[";
+		while(myNode != nullptr){
+			std::cout << myNode->value << ",";
+			myNode = myNode->next;
+		}
+		std::cout << "\b]" << std::endl;
+	}else{
+		std::cout << "\nList is empty" << std::endl;
+	}
 }
 
